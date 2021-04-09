5ad469de03a0d3df9cb7b6034955d8534f38349f
PIPIPINoBrain <70845572+PIPIPINoBrain@users.noreply.github.com>
2021-04-09 22:15:56 +0800

    Create 24.c

---
+++ 24.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	char* c[] = { "enter", "new", "point", "first" };
+	char** cp[] = { c + 3, c + 2, c + 1, c };
+	char*** cpp = cp;
+
+	printf("%s\n", **++cpp);
+	printf("%s\n", *-- * ++cpp + 3);
+	printf("%s\n", *cpp[-2] + 3);
+	printf("%s\n", cpp[-1][-1] + 1);
+	return 0;
+}
