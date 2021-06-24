79dbeae990f39e605c72f6701fca2c1e160e9768
PIPIPINoBrain <70845572+PIPIPINoBrain@users.noreply.github.com>
2021-06-24 22:27:54 +0800

    Create Dtest.c

---
+++ Dtest.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"DLIST.h"
+
+int main()
+{
+	DListData* phead = NULL;
+	DListPushback(&phead,1);
+	DListPushback(&phead, 2);
+	DListPushback(&phead, 3);
+	DListPushfront(&phead, 3);
+
+	DListShow(phead);
+	return 0;
+}
