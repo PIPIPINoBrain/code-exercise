5a1b56cd1b58f557f0d24d0723a30ac8705ef54f
PIPIPINoBrain <70845572+PIPIPINoBrain@users.noreply.github.com>
2021-06-24 22:29:36 +0800

    Create DLIST.h

---
+++ DLIST.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<stdio.h>
+
+typedef int datatype;
+typedef struct DListData 
+{
+	struct DListData* head;
+	datatype data;
+	struct DListData* next;
+
+}DListData;
+
+DListData* BuyListNode(datatype x);
+void DListPushback(DListData** pphead, datatype x);
+void DListPushfront(DListData** pphead, datatype x);
+void DListShow(DListData* phead);
