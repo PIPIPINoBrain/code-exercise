041e02ef5de32ed76037a402ed98d522da82229f
PIPIPINoBrain <70845572+PIPIPINoBrain@users.noreply.github.com>
2021-06-25 10:47:48 +0800

    Update and rename DLIST.h to 35.DLIST.h

--- 35.DLIST.h
+++ 35.DLIST.h
@@ -14,3 +14,5 @@ DListData* BuyListNode(datatype x);
 void DListPushback(DListData** pphead, datatype x);
 void DListPushfront(DListData** pphead, datatype x);
 void DListShow(DListData* phead);
+void DListPopback(DListData** pphead);
+void DListPopfront(DListData** pphead);
